f169b94f8d2508cf63fe0105c1c6d03aa8bcd577
klivanskyr <klivanskyr@gmail.com>
2024-06-05 16:20:12 -0400

    Updated seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -22,6 +22,59 @@ async function main() {
         email: 'test2@test2.com',
     }
   })
+
+  await prisma.book.upsert({
+    where: { key: 'testkey' },
+    update: {},
+    create: {
+        key: 'testkey',
+        title: 'Test Book',
+        author: 'Test Author',
+        isbn: 123456
+    }
+  })
+
+  await prisma.book.upsert({
+    where: { key: 'testkey2' },
+    update: {},
+    create: {
+        key: 'testkey2',
+        title: 'Test2 Book2',
+        author: 'Test2 Author2',
+        isbn: 12345
+    }
+  })
+
+  await prisma.book.upsert({
+    where: { key: 'testkey3' },
+    update: {},
+    create: {
+        key: 'testkey3',
+        title: 'Test3 Book3',
+        author: 'Test3 Author3',
+        isbn: 1234567
+    }
+  })
+
+  await prisma.review.upsert({
+    where: { id: 1},
+    update: {},
+    create: {
+        userId: 1,
+        bookId: 1,
+        rating: 5,
+        review: 'Great book!'
+    }
+  })
+
+  await prisma.user_book.upsert({
+    where: { id: 1},
+    update: {},
+    create: {
+        userId: 1,
+        bookId: 1,
+    }
+  })
 }
 
 main()
