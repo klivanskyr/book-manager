90b3d0e3675d4ce5d74ffd57f2beba6c57b70058
klivanskyr <klivanskyr@gmail.com>
2024-06-18 14:38:55 -0400

    Placed <SpeedInsights /> in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <Header />
         <ParentProvider>
           {children}
