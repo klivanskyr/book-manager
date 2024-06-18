627a57a8e76223d77110fe42a3099d994292d5f5
klivanskyr <klivanskyr@gmail.com>
2024-06-18 14:37:55 -0400

    Added speed insights with vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import Header from "./Header";
