34a5d606e327ce18e067dc89aebf0e39ed3c64ca
klivanskyr <klivanskyr@gmail.com>
2024-07-24 16:20:07 -0400

    Switch from .json file to secrets for sdk

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -1,11 +1,17 @@
-import { applicationDefault, initializeApp, getApps, cert } from 'firebase-admin/app';
+import { initializeApp, getApps, cert, ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
 let adminApp;
 
+const serviceAccount: ServiceAccount = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.NEXT_PULBIC_FIREBASE_CLIENT_EMAIL,
+};
+
 if (!getApps().some(app => app.name === 'admin')) {
     adminApp = initializeApp({
-        credential: applicationDefault(),
+        credential: cert(serviceAccount),
         databaseURL: "https://book-manager-3fe0f-default-rtdb.firebaseio.com",
     }, 'admin');
 } else {
